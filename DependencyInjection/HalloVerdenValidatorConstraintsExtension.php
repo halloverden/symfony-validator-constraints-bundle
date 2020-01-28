298dd87e9ee5553485b39173a277b2a1846b964d
Tom Roar Furunes <tom.roar@halloverden.no>
2020-01-28 18:00:27 +0100

    ADD: composer files and required bundle files

---
+++ DependencyInjection/HalloVerdenValidatorConstraintsExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace HalloVerden\ValidatorConstraintsBundle\DependencyInjection;
+
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class HalloVerdenValidatorConstraintsExtension extends Extension {
+
+  /**
+   * @inheritDoc
+   */
+  public function load(array $configs, ContainerBuilder $container) {
+    // TODO: Implement load() method.
+  }
+
+}
