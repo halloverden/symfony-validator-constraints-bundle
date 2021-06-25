33912cc56f6a3ea969ceebf9ddcecc7e58a81e8c
marco-giannetti <marco@halloverden.no>
2021-06-25 12:32:13 +0200

    EDIT: refactored constraint

--- Constraints/UniqueEntityPropertyValidator.php
+++ Constraints/UniqueEntityPropertyValidator.php
@@ -45,6 +45,7 @@ class UniqueEntityPropertyValidator extends ConstraintValidator {
   private function getConstraints(UniqueEntityProperty $constraint): array {
     return [
       new UniqueEntity([
+        'message'          => $constraint->message,
         'fields'           => $constraint->fields,
         'em'               => $constraint->em,
         'entityClass'      => $constraint->entityClass,
